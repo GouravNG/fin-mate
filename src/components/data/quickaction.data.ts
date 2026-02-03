1c9834a46835feab6a35bad774040d582ab752f3
Gourav <gouravgunaga555@gmail.com>
2026-02-03 21:29:22 +0530

    Quick Actions

---
+++ src/components/data/quickaction.data.ts
@@ -0,0 +1,35 @@
+import {
+  BriefcaseBusiness,
+  HandCoins,
+  Settings,
+  Split,
+  WavesArrowDown,
+  WavesArrowUp,
+} from "lucide-react"
+
+export const quickActions = [
+  {
+    title: "Send",
+    icon: WavesArrowUp,
+  },
+  {
+    title: "Request",
+    icon: WavesArrowDown,
+  },
+  {
+    title: "Split",
+    icon: Split,
+  },
+  {
+    title: "Settings",
+    icon: Settings,
+  },
+  {
+    title: "Monthly Report",
+    icon: HandCoins,
+  },
+  {
+    title: "Budget",
+    icon: BriefcaseBusiness,
+  },
+]
