72ecf91042988d594ede86b8f69fc85efa8a7b2d
Gourav <gouravgunaga555@gmail.com>
2026-02-15 19:56:12 +0530

    Linters and formats

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,33 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
+// For more info, see:
+// https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 
+import { defineConfig, globalIgnores } from 'eslint/config'
 import js from '@eslint/js'
 import globals from 'globals'
+import tseslint from 'typescript-eslint'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import storybook from 'eslint-plugin-storybook'
+import pluginRouter from '@tanstack/eslint-plugin-router'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
-export default defineConfig([globalIgnores(['dist']), {
-  files: ['**/*.{ts,tsx}'],
-  ignores: ["src/components/ui/**"],
-  extends: [
-    js.configs.recommended,
-    tseslint.configs.recommended,
-    reactHooks.configs.flat.recommended,
-    reactRefresh.configs.vite,
-  ],
-  languageOptions: {
-    ecmaVersion: 2020,
-    globals: globals.browser,
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    ignores: ['src/components/ui/**'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+      pluginRouter.configs['flat/recommended'],
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    ...pluginQuery.configs['flat/recommended'],
   },
-}, ...storybook.configs["flat/recommended"]])
+  ...storybook.configs['flat/recommended'],
+])
